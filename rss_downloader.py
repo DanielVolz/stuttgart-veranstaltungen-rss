44e04709833c212da9a92c1c3f2d3bd82728598e
Daniel Volz <mail@danielvolz.org>
2023-06-02 12:39:11 +0200

    refactor: Refactor rss generation

--- rss_downloader.py
+++ rss_downloader.py
@@ -15,32 +15,11 @@ import json
 import logging
 import html
 
-# Set up logging
-log_file = "rss_generator.log"
-log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), log_file)
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[logging.FileHandler(log_file_path), logging.StreamHandler()],
-)
-logger = logging.getLogger(__name__)
-
 
 def count_events(xml_file):
-    """counts the events in a xml file
-
-    Args:
-        xml_file (xml object): xml file with events
-
-    Returns:
-        int: count of the events
-    """ """"""
     tree = ET.parse(xml_file)
     root = tree.getroot()
-
     event_count = len(root.findall(".//item"))
-
     return event_count
 
 
@@ -111,185 +90,189 @@ def move_rss_log_files(destination_folder):
             logger.info("Moved file: %s", file_name)
 
 
-def generate_rss_feed(rss_name, rss_title, rss_category):
-    logger.info(
-        f"Start scraping the RSS category {rss_title} in to the file: {rss_name}."
-    )
-    today = datetime.date.today()
-    date_list = [today + datetime.timedelta(days=i) for i in range(7)]
-
-    locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
-
+def create_rss_element(rss_title):
     rss = ET.Element("rss", version="2.0")
     channel = ET.SubElement(rss, "channel")
     title = ET.SubElement(channel, "title")
     title.text = rss_title
+    return rss, channel
 
-    for date in date_list:
-        date_str = date.strftime("%Y-%m-%d")
-        url = f"https://www.stuttgart.de/service/veranstaltungen.php?form=eventSearch-1.form&sp:dateFrom[]={date_str}&sp:dateTo[]={date_str}&sp:categories[77306][]={rss_category}&action=submit"
 
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
+def create_date_list():
+    today = datetime.date.today()
+    date_list = [today + datetime.timedelta(days=i) for i in range(7)]
+    return date_list
 
-        link = ET.SubElement(channel, "link")
-        link.text = url
 
-        event_entries = soup.find_all(
-            "article",
-            class_="SP-Teaser SP-Grid__full SP-Teaser--event SP-Teaser--hasLinks SP-Teaser--textual",
+def fetch_event_entries(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    event_entries = soup.find_all(
+        "article",
+        class_="SP-Teaser SP-Grid__full SP-Teaser--event SP-Teaser--hasLinks SP-Teaser--textual",
+    )
+    return event_entries
+
+
+def process_event_entry(event_entry, url, channel):
+    ical_link, event = extract_event_info(event_entry, url)
+    event_html = build_event_html(event, ical_link)
+    add_event_to_channel(event, event_html, ical_link, channel)
+
+
+def extract_event_info(event_entry, url):
+    ical_link_element = event_entry.select_one(
+        ".SP-Teaser__links .SP-Link.SP-Iconized--left"
+    )
+    ical_link = urllib.parse.urljoin(url, ical_link_element["href"])
+    ical_link = urllib.parse.unquote(ical_link)
+    ical_response = requests.get(ical_link)
+    ical_data = ical_response.text
+    cal = ics.Calendar(ical_data)
+    event = list(cal.events)[0]
+    return ical_link, event
+
+
+def build_event_html(event, ical_link):
+    event_title = event.name
+    event_start = event.begin
+    event_end = event.end
+    event_location = event.location
+    event_description = event.description
+    event_url = event.url
+    event_categories = event.categories
+
+    germany_tz = pytz.timezone("Europe/Berlin")
+    event_start = event_start.astimezone(germany_tz)
+    event_end = event_end.astimezone(germany_tz)
+
+    event_start_time = event_start.strftime("%H:%M Uhr")
+    event_end_time = event_end.strftime("%H:%M Uhr")
+    event_date = event_start.strftime("%a, %d %b %Y")
+
+    tags_str = ", ".join(event_categories)
+
+    event_html = f"""
+    <div>"""
+
+    if event_url:
+        webpage_response = requests.get(event_url)
+        webpage_soup = BeautifulSoup(webpage_response.content, "html.parser")
+        picture_tag = webpage_soup.select_one("picture")
+        if picture_tag:
+            img_tag = picture_tag.find("img")
+            if img_tag:
+                image_url = urllib.parse.urljoin(event_url, img_tag.get("src"))
+                php_file_name = os.path.basename(event_url)
+                php_file_name = os.path.splitext(php_file_name)[0]
+                if php_file_name in image_url:
+                    event_html += f"<img src='{image_url}' alt='{event_title}'>"
+        else:
+            image_url = "https://www.stuttgart.de/openGraph-200x200.png"
+
+    event_html = f"""
+        <div style="display: flex;">
+        <div style="flex-shrink: 0; margin-right: 10px;">
+        <a href='{event_url}' target='_blank' style='float: left; margin-right: 10px;'><img src='{image_url}' alt='{event_title}' style='max-width: 200px;'></a>
+        </div>
+        <div>
+        <p>{event_description}</p>
+        <p><a href='{ical_link}' target='_blank'>{event_start_time} - {event_end_time}, {event_date} &#128197;</a></p>"""
+
+    if event_location:
+        google_maps_link = f"https://www.google.com/maps/search/?api=1&query={urllib.parse.quote(event_location)}"
+        event_html += (
+            f"<p><a href='{google_maps_link}' target='_blank'>{event_location}</a></p>"
         )
-        for event_entry in event_entries:
-            ical_link_element = event_entry.select_one(
-                ".SP-Teaser__links .SP-Link.SP-Iconized--left"
+
+    entrance_fee_element = webpage_soup.select_one(
+        ".SP-CallToAction__text .SP-Paragraph p"
+    )
+    if entrance_fee_element and entrance_fee_element.contents:
+        entrance_fee = entrance_fee_element.contents[0].strip()
+        event_html += f"<p>{entrance_fee}</p>"
+
+    if event_url:
+        extended_description_elements = webpage_soup.select(
+            ".SP-ArticleContent .SP-Text:not(.SP-Text--notice) .SP-Paragraph p:not(.SP-CallToAction__text .SP-Paragraph p)"
+        )
+
+        for element in extended_description_elements:
+            br_tags = element.find_all("br")
+            for br_tag in br_tags:
+                br_tag.replace_with("\n")
+
+        extended_description = "\n".join(
+            [element.text.strip() for element in extended_description_elements]
+        )
+
+        if extended_description:
+            event_html += f"<p>{extended_description}</p>"
+
+    # Find the targeted section element
+    exhibition_hours = webpage_soup.find(
+        "section",
+        class_="SP-Text SP-Text--boxed SP-Grid__full--background SP-Grid__full--backgroundHighlighted",
+    )
+
+    if exhibition_hours:
+        exhibition_hours_div = exhibition_hours.find("div")
+
+        if exhibition_hours_div:
+            exhibition_hours_html = "".join(map(str, exhibition_hours_div.contents))
+            event_html += f"<br/>{exhibition_hours_html}"
+
+    if tags_str:
+        event_html += f"<p><strong>Tags:</strong> {tags_str}</p>"
+
+    event_kicker_addon = webpage_soup.select_one(".SP-Kicker__addOn")
+    if event_kicker_addon:
+        event_kicker_addon = event_kicker_addon.text.strip()
+        event_html += f"<p><strong>Zusätzliche Info:</strong> {event_kicker_addon}</p>"
+
+    if event_url:
+        event_html += f"<p><a href='{event_url}' target='_blank'>{event_title} (link auf stuttgart.de)</a></p>"
+
+    event_html += "</div>"
+
+    return event_html
+
+
+def add_event_to_channel(event, event_html, ical_link, channel):
+    event_title = event.name
+    event_start = event.begin
+    pub_date = formatdate(event_start.datetime.timestamp(), usegmt=True)
+
+    event_exists = False
+    for existing_item in channel.findall("item"):
+        existing_title_element = existing_item.find("title")
+        if existing_title_element is None:
+            continue
+
+        existing_title = existing_title_element.text
+        existing_pub_date = existing_item.find("pubDate").text
+        # pub_date = formatdate(event_start.datetime.timestamp(), usegmt=True)
+        if existing_title == event_title and existing_pub_date == pub_date:
+            event_exists = True
+            # logger.info(f"Existing event: {existing_title}, date: {existing_pub_date}")
+            logger.info(
+                f"Skipping adding event: Duplicate event: {event_title}, date: {pub_date}"
             )
-            if ical_link_element:
-                ical_link = urllib.parse.urljoin(url, ical_link_element["href"])
-                ical_link = urllib.parse.unquote(ical_link)
-                ical_response = requests.get(ical_link)
-                ical_data = ical_response.text
-                cal = ics.Calendar(ical_data)
-                event = list(cal.events)[0]
+            break
 
-                event_title = event.name
-                event_start = event.begin
-                event_end = event.end
-                event_location = event.location
-                event_description = event.description
-                event_url = event.url
+    if event_exists:
+        return
 
-                event_categories = event.categories
-                tags_str = ", ".join(event_categories)
+    # Add the event to the XML channel
+    item = ET.SubElement(channel, "item")
+    ET.SubElement(item, "title").text = event_title
+    ET.SubElement(item, "description").text = html.unescape(event_html)
+    ET.SubElement(item, "link").text = event.url
+    ET.SubElement(item, "pubDate").text = pub_date
+    logger.info(f"Added to xml file: {event_title}, date: {pub_date}")
 
-                germany_tz = pytz.timezone("Europe/Berlin")
-                event_start = event_start.astimezone(germany_tz)
-                event_end = event_end.astimezone(germany_tz)
 
-                event_start_time = event_start.strftime("%H:%M Uhr")
-                event_end_time = event_end.strftime("%H:%M Uhr")
-                event_date = event_start.strftime("%a, %d %b %Y")
-
-                event_html = f"""
-                <div>"""
-
-                if event_url:
-                    webpage_response = requests.get(event_url)
-                    webpage_soup = BeautifulSoup(
-                        webpage_response.content, "html.parser"
-                    )
-                    picture_tag = webpage_soup.select_one("picture")
-                    if picture_tag:
-                        img_tag = picture_tag.find("img")
-                        if img_tag:
-                            image_url = urllib.parse.urljoin(
-                                event_url, img_tag.get("src")
-                            )
-                            php_file_name = os.path.basename(event_url)
-                            php_file_name = os.path.splitext(php_file_name)[0]
-                            if php_file_name in image_url:
-                                event_html += (
-                                    f"<img src='{image_url}' alt='{event_title}'>"
-                                )
-                    else:
-                        image_url = "https://www.stuttgart.de/openGraph-200x200.png"
-
-                event_html = f"""
-                    <div style="display: flex;">
-                    <div style="flex-shrink: 0; margin-right: 10px;">
-                    <a href='{event_url}' target='_blank' style='float: left; margin-right: 10px;'><img src='{image_url}' alt='{event_title}' style='max-width: 200px;'></a>
-                    </div>
-                    <div>
-                    <p>{event_description}</p>
-                    <p><a href='{ical_link}' target='_blank'>{event_start_time} - {event_end_time}, {event_date} &#128197;</a></p>"""
-
-                if event_location:
-                    google_maps_link = f"https://www.google.com/maps/search/?api=1&query={urllib.parse.quote(event_location)}"
-                    event_html += f"<p><a href='{google_maps_link}' target='_blank'>{event_location}</a></p>"
-
-                entrance_fee_element = webpage_soup.select_one(
-                    ".SP-CallToAction__text .SP-Paragraph p"
-                )
-                if entrance_fee_element and entrance_fee_element.contents:
-                    entrance_fee = entrance_fee_element.contents[0].strip()
-                    event_html += f"<p>{entrance_fee}</p>"
-
-                if event_url:
-                    extended_description_elements = webpage_soup.select(
-                        ".SP-ArticleContent .SP-Text:not(.SP-Text--notice) .SP-Paragraph p:not(.SP-CallToAction__text .SP-Paragraph p)"
-                    )
-
-                    for element in extended_description_elements:
-                        br_tags = element.find_all("br")
-                        for br_tag in br_tags:
-                            br_tag.replace_with("\n")
-
-                    extended_description = "\n".join(
-                        [
-                            element.text.strip()
-                            for element in extended_description_elements
-                        ]
-                    )
-
-                    if extended_description:
-                        event_html += f"<p>{extended_description}</p>"
-
-                # Find the targeted section element
-                exhibition_hours = webpage_soup.find(
-                    "section",
-                    class_="SP-Text SP-Text--boxed SP-Grid__full--background SP-Grid__full--backgroundHighlighted",
-                )
-
-                if exhibition_hours:
-                    exhibition_hours_div = exhibition_hours.find("div")
-
-                    if exhibition_hours_div:
-                        exhibition_hours_html = "".join(
-                            map(str, exhibition_hours_div.contents)
-                        )
-                        event_html += f"<br/>{exhibition_hours_html}"
-
-                if tags_str:
-                    event_html += f"<p><strong>Tags:</strong> {tags_str}</p>"
-
-                event_kicker_addon = event_entry.select_one(".SP-Kicker__addOn")
-                if event_kicker_addon:
-                    event_kicker_addon = event_kicker_addon.text.strip()
-                    event_html += f"<p><strong>Zusätzliche Info:</strong> {event_kicker_addon}</p>"
-
-                if event_url:
-                    event_html += f"<p><a href='{event_url}' target='_blank'>{event_title} (link auf stuttgart.de)</a></p>"
-
-                event_html += "</div>"
-
-                event_exists = False
-                for existing_item in channel.findall("item"):
-                    existing_title_element = existing_item.find("title")
-                    if existing_title_element is None:
-                        continue
-
-                    existing_title = existing_title_element.text
-                    existing_pub_date = existing_item.find("pubDate").text
-                    pub_date = formatdate(dt.timestamp(event_start), usegmt=True)
-
-                    if existing_title == event_title and existing_pub_date == pub_date:
-                        event_exists = True
-                        logger.info(
-                            f"Skipping duplicate event: {event_title}, date: {date}"
-                        )
-                        break
-
-                if event_exists:
-                    continue
-
-                item = ET.SubElement(channel, "item")
-                # Add the event to the XML file
-                ET.SubElement(item, "title").text = event_title
-                ET.SubElement(item, "description").text = html.unescape(event_html)
-                ET.SubElement(item, "link").text = event_url
-                pub_date = formatdate(dt.timestamp(event_start), usegmt=True)
-                ET.SubElement(item, "pubDate").text = pub_date
-                logger.info(f"Added to xml file: {event_title}, date: {date}")
-
+def write_rss_to_file(rss, rss_name):
     script_directory = os.path.dirname(os.path.abspath(__file__))
     rss_path = os.path.join(script_directory, rss_name)
 
@@ -299,31 +282,72 @@ def generate_rss_feed(rss_name, rss_title, rss_category):
 
     tree = ET.parse(rss_path)
     tree.write(rss_path, encoding="utf-8", xml_declaration=True)
-
     logger.info(f"{count_events(rss_path)} events added.")
     logger.info(f"RSS feed '{rss_name}' in {rss_path} generated successfully!")
 
 
-if __name__ == "__main__":
+def generate_rss_feed(rss_name, rss_title, rss_category):
+    logger.info(
+        f"Start scraping the RSS category {rss_title} in to the file: {rss_name}."
+    )
+    locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
+
+    rss, channel = create_rss_element(rss_title)
+    date_list = create_date_list()
+
+    for date in date_list:
+        date_str = date.strftime("%Y-%m-%d")
+        url = f"https://www.stuttgart.de/service/veranstaltungen.php?form=eventSearch-1.form&sp:dateFrom[]={date_str}&sp:dateTo[]={date_str}&sp:categories[77306][]={rss_category}&action=submit"
+
+        event_entries = fetch_event_entries(url)
+
+        for event_entry in event_entries:
+            process_event_entry(event_entry, url, channel)
+
+    write_rss_to_file(rss, rss_name)
+
+
+def setup_logging():
+    # Set up logging
+    log_file = "rss_generator.log"
+    log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), log_file)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[logging.FileHandler(log_file_path), logging.StreamHandler()],
+    )
+    logger = logging.getLogger(__name__)
+
+    return logger
+
+
+def main():
     destination_folder = "/home/pi/rss_feeds"
 
     logger.info(f"Starting scraping script. ##############")
-
-    # rss_name = "buehne_veranstaltungen.rss"
-    # rss_title = "Bühne - Stuttgart"
-    # rss_category = 79078
-    # generate_rss_feed(rss_name, rss_title, rss_category)
+    rss_name = "buehne_veranstaltungen.rss"
+    rss_title = "Bühne - Stuttgart"
+    rss_category = 79078
+    generate_rss_feed(rss_name, rss_title, rss_category)
 
     rss_name = "philo_veranstaltungen.rss"
     rss_title = "Literatur, Philosophie und Geschichte - Stuttgart"
     rss_category = 77317
     generate_rss_feed(rss_name, rss_title, rss_category)
 
-    # rss_name = "musik_veranstaltungen.rss"
-    # rss_title = "Musik - Stuttgart"
-    # rss_category = 79091
-    # generate_rss_feed(rss_name, rss_title, rss_category)
+    rss_name = "musik_veranstaltungen.rss"
+    rss_title = "Musik - Stuttgart"
+    rss_category = 79091
+    generate_rss_feed(rss_name, rss_title, rss_category)
 
-    move_rss_log_files(destination_folder)
+    logger.info("RSS feed generation completed.")
+
     update_nextcloud_news()
+    move_rss_log_files(destination_folder)
     logger.info(f"Stopping scraping script. ##############")
+
+
+if __name__ == "__main__":
+    logger = setup_logging()
+    main()
